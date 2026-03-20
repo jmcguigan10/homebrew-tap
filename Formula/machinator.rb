84e1cff2a7a7b490281d5ba0e3bc54033b9bd78d
John McGuigan <jmcguig1@vols.utk.edu>
2026-03-20 09:34:42 -0400

    Update machinator formula to 0.6.1

--- Formula/machinator.rb
+++ Formula/machinator.rb
@@ -3,8 +3,8 @@ class Machinator < Formula
 
   desc "Prompt-first control-plane CLI for ML workspaces and pipelines"
   homepage "https://github.com/jmcguigan10/Machinator"
-  url "https://github.com/jmcguigan10/Machinator/releases/download/v0.6.0/machinator-0.6.0.tar.gz"
-  sha256 "5a271cc1ab7792c154218256505b847e993a941df365cadc36cff21f78428484"
+  url "https://github.com/jmcguigan10/Machinator/releases/download/v0.6.1/machinator-0.6.1.tar.gz"
+  sha256 "c994b68d28396329688fdcd39457bc2515dd3e73173153821efed4f0a36d3d13"
   license "MIT"
 
   depends_on "python@3.12"
